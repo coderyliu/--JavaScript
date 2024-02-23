244a5c2605cdda807eb11f3bd061e0af2881c944
shuidi <shuidi@shuidideMacBook-Pro.local>
2024-02-23 18:03:20 +0800

    feat：事件总线

--- 手写js/常见工具函数/13_EventBus.js
+++ 手写js/常见工具函数/13_EventBus.js
@@ -1,28 +1,72 @@
 /**
- * @description 实现一个时间总线
+ * @description 实现一个事件总线，主要是用来组件之间的通信,也是发布订阅模式
  */
-class EventBus{
+class EventBus {
   constructor() {
-    this.eventBus = {}
+    this.eventBus = {};
   }
 
   // 事件订阅
-  on() {
-    
+  on(eventName, eventCallback, thisArg) {
+    if (typeof eventName !== "string")
+      throw new Error("the event name must be string type");
+    if (typeof eventCallback !== "function")
+      throw new Error("the event callback must be function");
+
+    let handlers = this.eventBus[eventName];
+    if (!handlers) {
+      handlers = [];
+      this.eventBus[eventName] = handlers;
+    }
+
+    handlers.push({
+      eventCallback,
+      thisArg,
+    });
   }
 
   // 事件发布
-  emit() {
+  emit(eventName, ...args) {
+    if (typeof eventName !== "string")
+      throw new Error("the event name must be string type");
 
+    const handlers = this.eventBus[eventName];
+    if (!handlers) return;
+    handlers.forEach((handler) => {
+      handler.eventCallback.apply(handler.thisArg, args);
+    });
   }
 
   // 事件取消订阅
-  off() {
+  off(eventName, eventCallback) {
+    if (typeof eventName !== "string")
+      throw new Error("the event name must be string type");
 
+    if (typeof eventCallback !== "function")
+      throw new Error("the event callback must be function");
+
+    const handlers = this.eventBus[eventName];
+    if (!handlers) return;
+    const newHandlers = [...handlers];
+    newHandlers.forEach((handler, index) => {
+      if (handler.eventCallback === eventCallback) {
+        newHandlers.splice(index, 1);
+      }
+    });
   }
 
   // 事件只触发一次
-  once() {
+  once(eventName, eventCallback, thisArg) {
+    if (typeof eventName !== "string")
+      throw new Error("the event name must be string type");
+    if (typeof eventCallback !== "function")
+      throw new Error("the event callback must be function");
 
+    const tempCallback = (...args) => {
+      this.off(eventName, tempCallback);
+      eventCallback.apply(thisArg, args);
+    };
+
+    return this.on(eventName, tempCallback, thisArg);
   }
 }
